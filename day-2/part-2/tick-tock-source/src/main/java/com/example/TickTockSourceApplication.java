7e3ffcd561c481d8085829d087a773cdc5c70da3
kbastani <kb@socialmoon.com>
2017-03-02 15:29:26 -0500

    Day 2 part 2

---
+++ day-2/part-2/tick-tock-source/src/main/java/com/example/TickTockSourceApplication.java
@@ -0,0 +1,37 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.stream.annotation.EnableBinding;
+import org.springframework.cloud.stream.messaging.Source;
+import org.springframework.integration.support.MessageBuilder;
+import org.springframework.scheduling.annotation.Scheduled;
+
+import java.util.Objects;
+
+@SpringBootApplication
+@EnableBinding(Source.class)
+public class TickTockSourceApplication {
+
+    private final Source source;
+    private String theClock = "tick";
+
+    public static void main(String[] args) {
+        SpringApplication.run(TickTockSourceApplication.class, args);
+    }
+
+    public TickTockSourceApplication(Source source) {
+        this.source = source;
+    }
+
+    @Scheduled(fixedRate = 2000L)
+    public void tickOrTock() {
+
+        source.output().send(MessageBuilder
+                .withPayload(theClock)
+                .build());
+
+        theClock = Objects.equals(theClock, "tick") ? "tock" : theClock;
+    }
+
+}
